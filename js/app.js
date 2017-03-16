541bfc84a6cedf56d7167faf8e4f33cf5ee1f3f7
sathish <sathish_kumar_93@yahoo.com>
2017-03-15 22:02:39 -0400

    commit 2

---
+++ js/app.js
@@ -0,0 +1,7 @@
+particlesJS.load('particlesExp', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles2', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
